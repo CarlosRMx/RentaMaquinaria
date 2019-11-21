0c1b996cbeeeefdb708c52549ed536f1aa243cf4
CRM16 <carlosene161998.cr@gmail.com>
2019-11-20 21:17:10 -0600

    V1

---
+++ mientorno/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\USUARIO\Documents\Django\proyectoCurso\mientorno\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
